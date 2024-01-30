708a37333449dcd51d0a2b7780d7cb615cbe6cb1
BUSHRA050 <bushraismail050@gmail.com>
2024-01-30 15:30:00 +0500

    first commit

---
+++ app.py
@@ -0,0 +1,21 @@
+import sys
+sys.path.append('D:/My Projects/Salman_Projects/Python')
+
+from flask import Flask, request, jsonify
+from prediction import chatbot_response
+
+
+app = Flask(__name__)
+
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json(force=True)
+    message = data['message']
+    response = chatbot_response(message)
+    return jsonify(response)
+
+
+if __name__ == '__main__':
+    app.run(port=5000)
